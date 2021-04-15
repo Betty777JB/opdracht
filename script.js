75dd813c0517a7eb51b8f0644584c05df9b7b007
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-15 22:41:08 +0200

    Opdracht: JS programma draaien op jouw computer + comments maken

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("Hallo Winc Academy studenten");
+let multiplication = 1230941 * 1823794; //dit is een grote som
+console.log(multiplication);/*Op deze manier zet je deze regels
+ code "uit".*/
+let division = 637263 / 54 ;
+console.log(division);
+/*Let op: runt JavaScript in Node? Dan is je terminal de console.
+ Runt JavaScript in je browser? Dan vind je de console in je developer tools. */
