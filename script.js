3d3eb818deb018e17cbc2a1e36d7500a5a77df2d
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-15 22:45:59 +0200

    einde js oefening

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let division = 637263 / 54 ;
 console.log(division);
 /*Let op: runt JavaScript in Node? Dan is je terminal de console.
  Runt JavaScript in je browser? Dan vind je de console in je developer tools. */
+ // Dit is een grote som
+
+
+
+ // oplossing
+ /*console.log(1230941 * 1823794);
+ console.log(637263 / 54);*/
+
+ /* Ik ben een blok comment
+ console.log(1230941 * 1823794);
+ console.log(637263 / 54);
+ */
+
+ // Code die niet "aan" staat zie je niet in de console.
+
+ // Let op: runt JavaScript in Node? Dan is je terminal de console.
+ // Runt JavaScript in je browser? Dan vind je de console in je developer tools.
